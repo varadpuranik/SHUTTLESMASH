d42123e45556d0cc7d459f9af5f582a95150eba9
varadpuranik <85935082+varadpuranik@users.noreply.github.com>
2025-08-08 14:01:52 +0530

    Add scoreboard and match API

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Scoreboard from '../components/Scoreboard';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Scoreboard />
+    </main>
+  );
+}
